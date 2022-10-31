d6cc7ba3156c773f879cd477ec2a95595461504d
Le Viet <leviet0115@gmail.com>
2022-10-31 16:38:59 +0200

    7.2 - 7.3

---
+++ src/components/Anecdote.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom";
+
+const Anecdote = ({ anecdotes }) => {
+  const id = useParams().id;
+  const anec = anecdotes.find((anec) => (anec.id = id));
+  return (
+    <div>
+      <h1>{anec.content}</h1>
+      <p>has {anec.votes} votes</p>
+      <p>for more info see: {anec.info}</p>
+    </div>
+  );
+};
+
+export default Anecdote;
