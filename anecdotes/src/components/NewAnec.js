69913fc0953f570893c5d179db5cec2dee564819
Le Viet <leviet0115@gmail.com>
2022-11-01 14:55:39 +0200

    7.6

---
+++ anecdotes/src/components/NewAnec.js
@@ -0,0 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import { useField } from "../hooks";
+
+const CreateNew = (props) => {
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0,
+    });
+    navigate("/");
+  };
+
+  const reset = () => {
+    content.onReset();
+    author.onReset();
+    info.onReset();
+  };
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit} onReset={reset}>
+        <div>
+          content
+          <input {...content} />
+        </div>
+        <div>
+          author
+          <input {...author} />
+        </div>
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+        <button type="submit">create</button>
+        <input type="reset" />
+      </form>
+    </div>
+  );
+};
+
+export default CreateNew;
